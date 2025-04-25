1409ffad4df00f5041b04a790d78e60c31278ca4
Fahad <fahadbinqaiser3@gmail.com>
2025-04-25 16:14:52 +0500

    Devise and views done
    Signup, Login and signout functionality done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!
   http_basic_authenticate_with name: "dhh", password: "secret", except: [ :index, :show ]
   def index
     @articles = Article.all
