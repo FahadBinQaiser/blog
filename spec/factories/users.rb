de8d55e09fff20215a42faee88f1200d20a598d0
Fahad <fahadbinqaiser3@gmail.com>
2025-06-12 18:22:49 +0500

    Article tests are now integrated with factorybot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,19 @@
-FactoryBot.define do 
+FactoryBot.define do
   factory :user do
-    name { "Pookie" }
-    email { "Faker::Internet.email" }
+    email { Faker::Internet.email }
+    password { "password" }
+  end
+
+  factory :article do
+    title { "Hello Pookie" }
+    body { Faker::Lorem.sentence }
+    status { "public" }
+    association :user
+  end
+
+  factory :comment do
+    body { Faker::Lorem.sentence }
+    association :article
+    association :user
   end
 end
