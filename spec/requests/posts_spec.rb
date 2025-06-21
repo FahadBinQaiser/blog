7aedf25be6d7228c2d06ca953e1a1a1a5767deee
Fahad <fahadbinqaiser3@gmail.com>
2025-06-21 16:20:23 +0500

    Ran rubocop

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do 
+  describe "GET /posts" do
     it "returns a successful response" do
     get posts_path
     expect(response).to have_http_status(:success)
@@ -9,14 +9,13 @@ RSpec.describe "Posts", type: :request do
   end
   describe "POST /posts" do
     let(:user) { create(:user) }
-    it "creates a post when a user is signed in" do 
+    it "creates a post when a user is signed in" do
       sign_in user
-      post posts_path, params: { post: { title: "Controller Tests" }}
+      post posts_path, params: { post: { title: "Controller Tests" } }
 
       expect(response).to redirect_to(post_path(Post.last))
     follow_redirect!
     expect(response.body).to include("Controller Tests")
-
     end
   end
 end
