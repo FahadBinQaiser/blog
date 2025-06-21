8ea482b0766be9f114f54d48771c8f43438b8c8e
Fahad <fahadbinqaiser3@gmail.com>
2025-06-21 15:25:03 +0500

    Model specs test done for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:post) { build(:post, user: user) }
+
+  it 'is with valid attributes' do
+    expect(post).to be_valid
+  end
+
+  it 'is invalid without a title' do
+  post.title = nil
+  expect(post).to_not be_valid
+  end
+
+    it 'is invalid with an empty title' do
+  post.title = ''
+  expect(post).to_not be_valid
+  end
+
+  it 'belongs to a user' do
+    expect(post.user).to eq(user)
+  end
+
+  it 'can have an attached image' do
+    post.image.attach(
+      io: File.open(Rails.root.join('spec', 'fixtures', 'test-image.jpg')),
+      filename: 'test-image.jpg',
+      content_type: 'image/jpeg'
+    )
+
+    expect(post.image).to be_attached
+  end
 end
