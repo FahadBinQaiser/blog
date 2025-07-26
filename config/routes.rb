5fded57348c94e8c7d06958d7874781f717385be
Fahad <fahadbinqaiser3@gmail.com>
2025-07-26 23:30:26 +0500

    modifying route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "articles#index"
- post "/dark_mode", to: "dark_mode#create", as: :dark_mode
-
+  resource :dark_mode, only: [ :create ], controller: "dark_mode"
   resources :articles do
     resources :comments
   end
