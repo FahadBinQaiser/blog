8b6d09f642ec09bcf85b936aa8803ecd41469bdc
Fahad <fahadbinqaiser3@gmail.com>
2025-07-28 00:47:56 +0500

    Likes setup done but with reload issue

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,7 +10,7 @@ class LikesController < ApplicationController
   def destroy
     @like = current_user.likes.find(params[:id])
     @like.destroy
-    redirect_to post
+    redirect_to post_path(@like.post)
   end
 
   private
