ac39eda9657aadb2cedc8b69338d69b2efb66232
Fahad <fahadbinqaiser3@gmail.com>
2025-08-12 12:28:06 +0500

    Fixing stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.limit(3)
   end
 
   def show
