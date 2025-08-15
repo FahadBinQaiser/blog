e85e36552410f910877f21b81ac7a285d4ca865f
Fahad <fahadbinqaiser3@gmail.com>
2025-08-16 01:30:12 +0500

    Fixed article showcase limit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
 
   def index
-    @articles = Article.all.limit(3)
+    @articles = Article.all
   end
 
   def show
