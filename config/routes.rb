35f55bbc5a767e3d85354444e1a83ff0f45bc536
Fahad <fahadbinqaiser3@gmail.com>
2025-08-15 12:34:14 +0500

    Changed from article to home page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "articles#index"
+  root "home#index"
   resources :likes, only: [ :create, :destroy ]
   resource :dark_mode, only: [ :create ], controller: "dark_mode"
   resources :articles do
