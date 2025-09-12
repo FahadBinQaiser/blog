db806ed3360e59d327dbc718428c5b5b3ca30370
Fahad <fahadbinqaiser3@gmail.com>
2025-09-12 16:12:16 +0500

    Adding tests for likes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:user) { create(:user) }
+    let(:post) { create(:post) }
+  it "increases the count by 1 when a like is created" do
+    like = Like.new(user: user, post: post)
+    expect { like.save }.to change(Like, :count).by(1)
+  end
+  it "decreases the count by 1 when a like is destroyed" do
+    like = Like.new(user: user, post: post)
+    like.save
+    expect { like.destroy }.to change(Like, :count).by(-1)
+  end
 end
