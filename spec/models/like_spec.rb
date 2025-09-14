87e5957743e61a11b17c9cfae743c27c17f234e9
Fahad <fahadbinqaiser3@gmail.com>
2025-09-14 18:45:23 +0500

    Like instance for model specs of likes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
     let(:user) { create(:user) }
     let(:post) { create(:post) }
-  it "increases the count by 1 when a like is created" do
-    like = Like.new(user: user, post: post)
-    expect { like.save }.to change(Like, :count).by(1)
-  end
+    let(:like) { Like.new(user: user, post: post) }
+
+    it "increases the count by 1 when a like is created" do
+      expect { like.save }.to change(Like, :count).by(1)
+    end
   it "decreases the count by 1 when a like is destroyed" do
-    like = Like.new(user: user, post: post)
     like.save
     expect { like.destroy }.to change(Like, :count).by(-1)
   end
