14828a5f7095a5c552b481797ace694ee521f07a
Fahad <fahadbinqaiser3@gmail.com>
2025-09-15 20:44:39 +0500

    Like spec test

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
+  let(:user) { create(:user) }
+  let(:postBlog) { create(:post) }
+
+  before { sign_in user }
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'creates a like on the first click of the post' do
+      expect {
+        post likes_path, params: { like: { post_id: postBlog.id } }
+      }.to change(Like, :count).by(1)
+    end
   end
 end
