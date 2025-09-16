f97c2ace489fd259a5a901e5783f9f35cad64c23
Fahad <fahadbinqaiser3@gmail.com>
2025-09-16 10:59:20 +0500

    Changed instance name

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
   let(:user) { create(:user) }
-  let(:postBlog) { create(:post) }
+  let(:posts) { create(:post) }
+  let(:like) { create(:like, user_id: user.id, post_id: posts.id) }
 
   before { sign_in user }
   describe "GET /index" do
     it 'creates a like on the first click of the post' do
       expect {
-        post likes_path, params: { like: { post_id: postBlog.id } }
+        post likes_path, params: { like: { post_id: posts.id } }
       }.to change(Like, :count).by(1)
     end
   end
