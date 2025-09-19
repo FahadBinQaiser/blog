6221d472de0b4e9d11bbdb4875d2d02c295080c3
Fahad <fahadbinqaiser3@gmail.com>
2025-09-19 17:00:41 +0500

    Seperated tests with describe blocks

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -3,14 +3,24 @@ require 'rails_helper'
 RSpec.describe "Likes", type: :request do
   let(:user) { create(:user) }
   let(:posts) { create(:post) }
-  let(:like) { create(:like, user_id: user.id, post_id: posts.id) }
 
-  before { sign_in user }
-  describe "GET /index" do
+  before do
+    sign_in user
+  end
+  describe "POST /index" do
     it 'creates a like on the first click of the post' do
       expect {
         post likes_path, params: { like: { post_id: posts.id } }
       }.to change(Like, :count).by(1)
     end
+  end
+  describe "DELETE /likes/:id" do
+    it 'destroys a like on the second click of the post' do
+      post likes_path, params: { like: { post_id: posts.id } }
+      like = Like.last
+      expect {
+         delete like_path(like.id)
+      }.to change(Like, :count).by(-1)
+  end
   end
 end
