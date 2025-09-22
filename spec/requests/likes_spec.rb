2865d0bf24a0f2f14d7c71f3840c08abc5398fdd
Fahad <fahadbinqaiser3@gmail.com>
2025-09-22 16:57:52 +0500

    Improved code quality, applied DRY rule

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -8,40 +8,40 @@ RSpec.describe "Likes", type: :request do
     sign_in user
   end
   describe "POST /likes" do
-    it 'creates a like on the first click of the post' do
-      expect {
-        post likes_path, params: { like: { post_id: posts.id } }
-      }.to change(Like, :count).by(1)
+    subject(:create_like) { post likes_path, params: { like: { post_id: posts.id } } }
+    context "when a like is created" do
+      it 'creates a like on the first click of the post' do
+      expect { create_like }.to change(Like, :count).by(1)
+      end
+      it 'redirects when a like is successfully created' do
+        create_like
+        expect(response).to redirect_to posts_path
+      end
     end
-    it 'redirects when a like is successfully created' do
-      post likes_path, params: { like: { post_id: posts.id } }
-      expect(response).to redirect_to posts_path
-    end
-    it 'does not create a second like for the same user and post' do
-      post likes_path, params: { like: { post_id: posts.id } }
-      expect {
-        post likes_path, params: { like: { post_id: posts.id } }
-    }.not_to change(Like, :count)
-    end
-    it 'redirects when a like is not created' do
-      post likes_path, params: { like: { post_id: posts.id } }
-      post likes_path, params: { like: { post_id: posts.id } }
-      expect(response).to redirect_to(posts_path)
+
+    context "when the user already liked the post" do
+      before { create_like }
+      it 'does not create a second like for the same user and post' do
+        expect { create_like }.not_to change(Like, :count)
+      end
+      it 'redirects when a like is not created' do
+        create_like
+        expect(response).to redirect_to(posts_path)
+      end
     end
   end
   describe "DELETE /likes/:id" do
+    let!(:like) { user.likes.create!(post_id: posts.id) }
+
     it 'destroys a like on the second click of the post' do
-      post likes_path, params: { like: { post_id: posts.id } }
-      like = Like.last
       expect {
-         delete like_path(like.id)
+        delete like_path(like.id)
       }.to change(Like, :count).by(-1)
     end
-    it 'redirects when a like is destroyed on second click' do
-      post likes_path, params: { like: { post_id: posts.id } }
-      like = Like.last
+
+    it 'redirects when a like is destroyed' do
       delete like_path(like.id)
-      expect(response).to redirect_to posts_path
+      expect(response).to redirect_to(posts_path)
     end
   end
 end
