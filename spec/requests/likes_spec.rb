941cd638be8ccd43d42929b807ec51f03a342816
Fahad <fahadbinqaiser3@gmail.com>
2025-09-22 14:55:54 +0500

    Controller tests for likes almost done

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -19,6 +19,15 @@ RSpec.describe "Likes", type: :request do
         post likes_path, params: { like: { post_id: posts.id } }
     }.not_to change(Like, :count)
     end
+    it 'redirects when a like is successfully created' do
+      post likes_path, params: { like: { post_id: posts.id } }
+      expect(response).to redirect_to posts_path
+    end
+    it 'redirects when a like is not created' do
+      post likes_path, params: { like: { post_id: posts.id } }
+      post likes_path, params: { like: { post_id: posts.id } }
+      expect(response).to redirect_to(posts_path)
+    end
   end
   describe "DELETE /likes/:id" do
     it 'destroys a like on the second click of the post' do
@@ -28,5 +37,11 @@ RSpec.describe "Likes", type: :request do
          delete like_path(like.id)
       }.to change(Like, :count).by(-1)
   end
+    it 'redirects when a like is destroyed on second click' do
+      post likes_path, params: { like: { post_id: posts.id } }
+      like = Like.last
+      delete like_path(like.id)
+      expect(response).to redirect_to posts_path
+    end
   end
 end
