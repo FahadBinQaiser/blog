cfb4355b2192f1a638669c3bacaa6fddfbf46947
Fahad <fahadbinqaiser3@gmail.com>
2025-09-22 14:08:07 +0500

    Writing tests for controller

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe "Likes", type: :request do
         post likes_path, params: { like: { post_id: posts.id } }
       }.to change(Like, :count).by(1)
     end
+    it 'does not create a second like for the same user and post' do
+      post likes_path, params: { like: { post_id: posts.id } }
+      expect {
+        post likes_path, params: { like: { post_id: posts.id } }
+    }.not_to change(Like, :count)
+    end
   end
   describe "DELETE /likes/:id" do
     it 'destroys a like on the second click of the post' do
