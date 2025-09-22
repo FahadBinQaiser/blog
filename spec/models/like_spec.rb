c846a5e9e619dafff0ff124a25f1659db8f6d826
Fahad <fahadbinqaiser3@gmail.com>
2025-09-22 15:04:22 +0500

    Formatting improved

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-    let(:user) { create(:user) }
-    let(:post) { create(:post) }
-    let(:like) { Like.new(user: user, post: post) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:like) { Like.new(user: user, post: post) }
 
-    it "increases the count by 1 when a like is created" do
-      expect { like.save }.to change(Like, :count).by(1)
-    end
+  it "increases the count by 1 when a like is created" do
+    expect { like.save }.to change(Like, :count).by(1)
+  end
   it "decreases the count by 1 when a like is destroyed" do
     like.save
     expect { like.destroy }.to change(Like, :count).by(-1)
