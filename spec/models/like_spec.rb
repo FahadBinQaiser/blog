72285a4b77d71b4eda49a4f4e65cf1a51d1bd570
Fahad <fahadbinqaiser3@gmail.com>
2025-09-22 16:19:20 +0500

    Tests for likes model file

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -4,12 +4,32 @@ RSpec.describe Like, type: :model do
   let(:user) { create(:user) }
   let(:post) { create(:post) }
   let(:like) { Like.new(user: user, post: post) }
-
-  it "increases the count by 1 when a like is created" do
-    expect { like.save }.to change(Like, :count).by(1)
+  describe "validations" do
+    it 'is valid with a user and a post' do
+      expect(like).to be_valid
+    end
+    it 'is not valid without a user' do
+      like = Like.new(user: user)
+      expect(like).not_to be_valid
+    end
+    it 'is not valid without a post' do
+      like = Like.new(post: post)
+      expect(like).not_to be_valid
+    end
+    it "does not allow the same user to like the same post twice" do
+    Like.create!(user: user, post: post)
+    duplicate_like = Like.new(user: user, post: post)
+    expect(duplicate_like).not_to be_valid
+    end
   end
-  it "decreases the count by 1 when a like is destroyed" do
-    like.save
-    expect { like.destroy }.to change(Like, :count).by(-1)
+
+  describe "database tests" do
+    it "increases the count by 1 when a like is created" do
+    expect { like.save }.to change(Like, :count).by(1)
+    end
+    it "decreases the count by 1 when a like is destroyed" do
+      like.save
+      expect { like.destroy }.to change(Like, :count).by(-1)
+    end
   end
 end
