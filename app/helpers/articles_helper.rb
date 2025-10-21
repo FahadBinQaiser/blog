bb7884705b72372a1e6f1a4b2a7e45a00521faca
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-10-21 15:54:17 +0500

    Added helper code for bg gradient

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+  def article_bg_gradient
+      "relative bg-gradient-to-br from-emerald-50 to-green-100 dark:from-gray-900 dark:to-gray-800 rounded-3xl p-8 shadow-xl hover:scale-105 hover:-translate-y-10 transition-transform duration-500 flex flex-col h-full"
+  end
 end
